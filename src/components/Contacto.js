839fc1a32dfd949d21406076270f5fe411a01bcf
CristianSivilla <cristiansivillacalvo@gmail.com>
2024-01-29 20:10:45 +0100

    map

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import '../styles/Contacto.css';
-
 import About from '../components/About';
+import Map from '../components/Map';
+
 
 const Contacto = () => {
   return (
     <div>
         <div className="contact-screen">
         <About/>
-
-
+        <Map/>
         </div>
       
     </div>

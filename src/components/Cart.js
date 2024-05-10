3e0c3540a227192b7572e00f67f59f6f084cb45e
CristianSivilla <cristiansivillacalvo@gmail.com>
2024-05-10 10:27:14 +0200

    a

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Modal from 'react-modal'; 
+import { useHistory } from 'react-router-dom'; 
+import '../styles/Cart.css';
 
 const Cart = () => {
   const [cartItems, setCartItems] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(true); 
+  const history = useHistory(); 
 
   useEffect(() => {
     const fetchCartItems = async () => {
@@ -17,21 +22,44 @@ const Cart = () => {
     fetchCartItems();
   }, []);
 
+  const eliminarProducto = async (productoId) => {
+    try {
+      await axios.delete(`http://localhost:3001/api/carrito/${productoId}`);
+      const updatedCartItems = cartItems.filter(item => item.productoId !== productoId); // Corregir la comparación aquí
+      setCartItems(updatedCartItems);
+      alert('Producto eliminado correctamente del carrito');
+    } catch (error) {
+      console.error('Error al eliminar el producto del carrito:', error);
+    }
+  };
+
+  const closeModalAndRedirect = () => {
+    setModalIsOpen(false);
+    history.push('/'); 
+  };
+
   return (
-    <div>
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModalAndRedirect}
+      ariaHideApp={false} 
+    >
       <h2>Carrito</h2>
       <ul>
         {cartItems.map((item, index) => (
           <li key={index}>
-            <div>
-              <h3>{item.nombre}</h3> {/* Ajusta según la estructura de datos de tu carrito */}
+            <div className='caja-carrito'>
+              <img className='imagen-carrito' src={require(`../assets/${item.nombre}.jpg`)} alt={item.nombre} />
+              <h3>{item.nombre}</h3>
               <p>Precio: {item.precio}</p>
               <p>Cantidad: {item.cantidad}</p>
+              <button className='carrito-button' onClick={() => eliminarProducto(item.productoId)}>Eliminar</button>
             </div>
           </li>
         ))}
       </ul>
-    </div>
+      <button className='boton-carritomodal' onClick={closeModalAndRedirect}>Cerrar</button>
+    </Modal>
   );
 };
 
--- src/components/Producto.js
+++ src/components/Producto.js
@@ -5,25 +5,26 @@ import axios from 'axios';
 
 function Producto() {
   const location = useLocation();
-  const { nombre, imagen, descripcion, precio } = location.state;
+  const { nombre, imagen, descripcion, precio, id } = location.state; // Asegúrate de que el ID del producto esté disponible
   const [userId, setUserId] = useState(null);
-
+  const [agregadoAlCarrito, setAgregadoAlCarrito] = useState(false); 
   useEffect(() => {
     setUserId(2);
   }, []);
 
   const handleCarritoClick = async () => {
-    const productId = 1; 
+    const productoId = 1; 
     const usuarioId = 3; 
 
     try {
       const response = await axios.post('http://localhost:3001/api/agregar', {
         usuarioId: usuarioId,  
-        productoId: productId,
+        productoId: productoId,
         cantidad: 2
       });
 
       console.log(response.data); 
+      setAgregadoAlCarrito(true); 
     } catch (error) {
       console.error('Error al agregar el producto al carrito:', error);
     }
@@ -38,6 +39,8 @@ function Producto() {
           <p className='descripcion-producto'>{descripcion}</p>
           <h4 className='precio-producto'>{precio}</h4>
           <button className='carrito-producto' onClick={handleCarritoClick}>Añadir al Carrito</button>
+          {agregadoAlCarrito && <p className="mensaje-carrito">Producto añadido correctamente al carrito</p>}
+          <button>pagar</button>
         </div>
       </div>
     </div>
